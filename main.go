7b3a0718bf1a7a8860ec3b313a60d923b40a8978
Artem Zhukov <a.izh@inbox.ru>
2024-12-10 17:18:22 +0300

    Add comments to main.go

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+//случайный коммент
 import (
 	"fmt"
 )
@@ -8,6 +9,7 @@ func Add(a, b int) int {
 	return a + b
 }
 
+// второй случайный коммент
 func main() {
 	fmt.Println("Я домашка")
 }
